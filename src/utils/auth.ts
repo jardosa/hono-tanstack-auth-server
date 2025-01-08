852a05d76ece5e5bf79b06dc0bf77b260eeeb6b5
John Ardosa <jardosa@live.com>
2025-01-08 13:55:57 +0800

    feat: add google signin

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -38,6 +38,10 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }
   },
   advanced: {
