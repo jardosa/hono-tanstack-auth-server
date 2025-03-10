eb9ac7103f292928799ff8d35a7f90919c0cff09
John Ardosa <jardosa@live.com>
2025-03-10 19:57:15 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,36 @@ import { showRoutes } from 'hono/dev';
 import { lead } from './modules/leads/controllers/lead.route.js';
 import migrator from './database/migrate.js';
 
-export const app = new Hono().basePath('/api/')
+export const app = new Hono<{
+	Variables: {
+		user: typeof auth.$Infer.Session.user | null,
+		session: typeof auth.$Infer.Session.session | null
+	}
+}>().basePath('/api/')
+
+app.use("*", async (c, next) => {
+	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+	if (!session) {
+		c.set("user", null);
+		c.set("session", null);
+		return next();
+	}
+
+	c.set("user", session.user);
+	c.set("session", session.session);
+	return next();
+});
+
+app.use('*', cors({
+	origin: "http://localhost:3001", // replace with your origin
+	allowHeaders: ["Cookie", "Content-Type", "Authorization", "credential"],
+	allowMethods: ["POST", "GET", "OPTIONS"],
+	exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+	maxAge: 600,
+	credentials: true,
+}))
+
 
 app.use(
 	"/auth/**", // or replace with "*" to enable cors for all routes
@@ -24,6 +53,28 @@ app.on(["POST", "GET"], "/auth/**", (c) => auth.handler(c.req.raw));
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!')
+})
+
+app.get("/session", async (c) => {
+	const session = c.get("session")
+	const user = c.get("user")
+
+	console.log(session, user)
+	if (!user) return c.body(null, 401);
+
+	return c.json({
+		session,
+		user
+	});
+});
+
+app.use('/lead/*', async (c, next) => {
+
+	const user = c.get("user")
+	if (!user) return c.body(null, 401);
+	console.log({ user })
+
+	return next()
 })
 
 app.route('/lead', lead)
